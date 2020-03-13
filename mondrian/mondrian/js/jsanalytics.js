bbdd8fdb252941f81f476c7144f99554dbcce6a2
danipedro2006 <danipedro2006@yahoo.com>
2020-03-13 20:33:19 +0200

    add new file

---
+++ mondrian/mondrian/js/jsanalytics.js
@@ -0,0 +1,10 @@
+var _gaq = _gaq  [];
+_gaq.push(['_setAccount', 'UA-37775884-1']);
+_gaq.push(['_setDomainName', 'jenniferdewalt.com']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'textjavascript'; ga.async = true;
+  ga.src = ('https' == document.location.protocol  'httpsssl'  'httpwww') + '.google-analytics.comga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
